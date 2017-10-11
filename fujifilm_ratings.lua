3bc2ca97bbe9ccab2bdca303b49dbc5aaee44374
Ben Mendis <bmendis@novetta.com>
2017-10-10 20:56:49 -0400

    Adding the license text to the repo

--- fujifilm_ratings.lua
+++ fujifilm_ratings.lua
@@ -1,8 +1,24 @@
--- fujifilm_ratings-0.1
--- Apply Fujifilm in-camera ratings during import
+--[[ fujifilm_ratings-0.1
 
--- Author: Ben Mendis (ben.mendis@gmail.com)
--- License: GPLv2
+Support for importing Fujifilm in-camera ratings in darktable.
+
+Copyright (C) 2017 Ben Mendis <ben.mendis@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+--]]
 
 darktable = require "darktable"
 
