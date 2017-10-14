81b4f3f523547e1d76901d75673da90951e39d6b
Ben Mendis <ben.mendis@gmail.com>
2017-10-14 11:12:22 -0400

    Adding the libs from github:darktable/lua_scripts

--- fujifilm_ratings.lua
+++ fujifilm_ratings.lua
@@ -23,34 +23,40 @@ Dependencies:
 
 --]]
 
-darktable = require "darktable"
+local dt = require "darktable"
+local df = require "lib/dtutils.file"
+dt.configuration.check_version(..., {4,0,0})
+
+if not df.check_if_bin_exists("exiftool-dne") then
+	print('Did not find exiftool-dne')
+end
 
 local function detect_rating(event, image)
 	local RAF_filename = tostring(image)
 	local JPEG_filename = string.gsub(RAF_filename, "%.RAF$", ".JPG")
 	local command = "exiftool -Rating " .. JPEG_filename
-	darktable.print_error(command)
+	dt.print_error(command)
 	local output = io.popen(command)
 	local jpeg_result = output:read("*all")
 	output:close()
 	if string.len(jpeg_result) > 0 then
 		jpeg_result = string.gsub(jpeg_result, "^Rating.*(%d)", "%1")
 		image.rating = tonumber(jpeg_result)
-		darktable.print_error("Using JPEG Rating: " .. tostring(jpeg_result))
+		dt.print_error("Using JPEG Rating: " .. tostring(jpeg_result))
 		return
 	end
 	command = "exiftool -Rating " .. RAF_filename
-	darktable.print_error(command)
+	dt.print_error(command)
 	output = io.popen(command)
 	local raf_result = output:read("*all")
 	output:close()
 	if string.len(raf_result) > 0 then
 		raf_result = string.gsub(raf_result, "^Rating.*(%d)", "%1")
 		image.rating = tonumber(raf_result)
-		darktable.print_error("Using RAF Rating: " .. tostring(raf_result))
+		dt.print_error("Using RAF Rating: " .. tostring(raf_result))
 	end
 end
 
-darktable.register_event("post-import-image", detect_rating)
+dt.register_event("post-import-image", detect_rating)
 
-darktable.print_error("fujifilm_ratings loaded.")
+dt.print_error("fujifilm_ratings loaded.")
