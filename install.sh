6c645a5a91e5ac1846f1b42b2d216422caab5545
Ben Mendis <bmendis@novetta.com>
2018-02-13 15:19:16 -0500

    Improving the isntall script

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-LOCAL_DIR=$(dirname "$0")
+LOCAL_DIR=$(dirname "$BASH_SOURCE")
 
 DT_DIR="$HOME/.config/darktable"
 LUA_DIR="$DT_DIR/lua"
 
 mkdir -p "$LUA_DIR"
-cp "$LOCAL_DIR/fujifilm_ratings.lua" "$LUA_DIR"
-echo 'require "fujifilm_ratings"' >> "$DT_DIR/luarc"
+ln -sf "$LOCAL_DIR/lib" "$LUA_DIR/lib"
+ln -sf "$LOCAL_DIR/fujifilm_ratings.lua" "$LUA_DIR"
+
+REQUIRE_LINE='require "fujifilm_ratings"'
+if ! grep -q "$REQUIRE_LINE" "$DT_DIR/luarc"; then
+    echo "$REQUIRE_LINE" >> "$DT_DIR/luarc"
+fi
